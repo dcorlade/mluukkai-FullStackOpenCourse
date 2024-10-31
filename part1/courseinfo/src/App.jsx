07d33e90dc9db5ee541fc16cda877a79dfdc6918
Denis Corlade <corladedenis19@gmail.com>
2024-10-31 15:29:50 +0200

    Part 1 ex1.7

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -11,18 +11,30 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
 
   const onGood = () => {
     console.log("good value: ", good)
     setGood(good + 1)
+    setAll(all + 1)
   }
   const onNeutral = () => {
     console.log("neutral value: ", neutral)
     setNeutral(neutral + 1)
+    setAll(all + 1)
   }
-  const onBad = (vlue) => {
+  const onBad = () => {
     console.log("bad value: ", bad)
     setBad(bad + 1)
+    setAll(all + 1)
+  }
+
+  const average = () => {
+    return (good - bad) / all
+  }
+
+  const positive = () => {
+    return good / all * 100
   }
 
   return (
@@ -35,6 +47,9 @@ const App = () => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {average()}</div>
+      <div>positive {positive()} %</div>
     </div>
   )
 }
