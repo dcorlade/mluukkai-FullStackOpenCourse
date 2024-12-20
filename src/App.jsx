fc4041459b2c28161337eab58e3062e79d9ff8ad
Denis Corlade <corladedenis19@gmail.com>
2024-12-20 16:18:37 +0200

    5.1-5.2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +16,107 @@ const App = () => {
     )
   }, [])
 
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={({ target }) => setUsername(target.value)}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={({ target }) => setPassword(target.value)}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const lougoutForm = () => (
+    <div>
+      <form onSubmit={handleLogout}>
+        <p style={{ display: 'inline', marginRight: '10px' }}>
+          {user.name} logged-in
+        </p>
+        <button type="submit" style={{ display: 'inline' }}>logout</button>
+      </form>
+      <div>
+        <h2>blogs</h2>
+        {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+        )}
+      </div>
+    </div>
+  )
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      console.log('Logging in')
+      const user = await loginService.login({
+        username, password,
+      })
+
+      window.localStorage.setItem(
+        'loggedBlogappUser', JSON.stringify(user)
+      )
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setTimeout(() => {
+        console.log('Wrong credentials')
+      }, 5000)
+    }
+  }
+
+  const handleLogout = async (event) => {
+    event.preventDefault()
+
+    try {
+      console.log('Logging out')
+
+      window.localStorage.removeItem('loggedBlogappUser')
+
+      setUser(null)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      setTimeout(() => {
+        console.log('Unable to logout')
+      }, 5000)
+    }
+  }
+
   return (
     <div>
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {user === null ?
+        <div>
+          <h2>Log in to application</h2>
+          {loginForm()}
+        </div>
+        :
+        lougoutForm()
+      }
     </div>
   )
 }
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,9 +1,18 @@
 import axios from 'axios'
 const baseUrl = '/api/blogs'
 
+let token = null
+
+const setToken = newToken => {
+  token = `Bearer ${newToken}`
+}
+
 const getAll = async () => {
   try {
-    const response = await axios.get(baseUrl)
+    const config = {
+      headers: { Authorization: token },
+    }
+    const response = await axios.get(baseUrl, config)
     return response.data
   } catch (error) {
     console.error('Error fetching data:', error)
@@ -11,4 +20,4 @@ const getAll = async () => {
   }
 }
 
-export default { getAll }
+export default { getAll, setToken }
