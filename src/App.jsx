8c46f9021414daacec9d91189d90f844bcb7331a
Denis Corlade <corladedenis19@gmail.com>
2024-12-25 17:37:46 +0200

    5.6-5.7

--- src/App.jsx
+++ src/App.jsx
@@ -146,7 +146,9 @@ const App = () => {
           <div>
             <h2>blogs</h2>
             {blogs.map(blog =>
-              <Blog key={blog.id} blog={blog} />
+              <div key={blog.id}>
+                <Blog blog={blog} />
+              </div>
             )}
           </div>
         </div>
--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,29 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import Togglable from './Togglable'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  return (
+    <div style={blogStyle}>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <span>{blog.title} {blog.author}</span>
+        <Togglable buttonLabel='view' style={{ marginLeft: '30px' }}>
+          <p>{blog.url}</p>
+          <p>
+            likes {blog.likes}
+            <button>like</button>
+          </p>
+          <p>{blog.user.name}</p>
+        </Togglable>
+      </div>
+    </div>
+  )
+}
 
 export default Blog
